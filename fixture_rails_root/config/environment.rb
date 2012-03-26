25a3b7530e116703da7765035a2a20c4cf019304
Ben Holley <ben@isotope11.com>
2012-03-26 11:04:50 -0500

    [mongoid] remove fixture_rails_root

--- fixture_rails_root/config/environment.rb
+++
@@ -1,5 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-require File.expand_path('../../../lib/plutus', __FILE__)
-# Initialize the rails application
-FixtureRailsRoot::Application.initialize!
