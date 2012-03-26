5f327cc774fc1a989e78dc542ba09b24998593cd
Ben Holley <ben@isotope11.com>
2012-03-26 11:05:05 -0500

    [mongoid] add spec/dummy

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'd4a8a1cea0d1c7ae12b2a9076f3259856a2cea4d56bd30939e3174aa047047fdddf3476025f2fb8e3b504e53e91e6546db9812c67759e12ab6b2381c41719872'
