b6c10e97d414b6d05c52bbcb02f3f57c6dec2107
Nicholas Fine <nicholas.fine@gmail.com>
2012-04-12 13:26:36 -0500

    DRY up Transaction code into modules

---
+++ app/models/plutus/extensions/transaction.rb
@@ -0,0 +1,15 @@
+module Plutus
+  module Extensions
+    module Transaction
+      extend ActiveSupport::Concern
+
+      included do
+        belongs_to :commercial_document, :polymorphic => true
+        belongs_to :credit_account, :class_name => "Plutus::Account"
+        belongs_to :debit_account, :class_name => "Plutus::Account"
+        validates_presence_of :credit_account, :debit_account, :amount, :description
+        validates_associated :credit_account, :debit_account
+      end
+    end
+  end
+end
