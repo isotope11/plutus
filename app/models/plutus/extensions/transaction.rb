9fa149440d3c15aff4fed5f0d7d5fa53d275fc1d
Nicholas Fine <nicholas.fine@gmail.com>
2012-04-12 13:30:52 -0500

    formatting cleanup

--- app/models/plutus/extensions/transaction.rb
+++ app/models/plutus/extensions/transaction.rb
@@ -5,10 +5,11 @@ module Plutus
 
       included do
         belongs_to :commercial_document, :polymorphic => true
-        belongs_to :credit_account, :class_name => "Plutus::Account"
-        belongs_to :debit_account, :class_name => "Plutus::Account"
+        belongs_to :credit_account,      :class_name => "Plutus::Account"
+        belongs_to :debit_account,       :class_name => "Plutus::Account"
+
         validates_presence_of :credit_account, :debit_account, :amount, :description
-        validates_associated :credit_account, :debit_account
+        validates_associated  :credit_account, :debit_account
       end
     end
   end
